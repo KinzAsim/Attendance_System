21cf70a09b94f27bb1e7471c45a3ea65ad168e82
KinzAsim <kinzasim383@gmail.com>
2020-11-17 18:00:08 +0500

    settings commit

--- src/modules/attendenceSystem/logs/logs.js
+++ src/modules/attendenceSystem/logs/logs.js
@@ -1,175 +1,68 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import {
     StyleSheet,
     View,
-    Platform,
     Text,
-    FlatList,
-    TouchableOpacity,
-    Image,
+    StatusBar,
     SafeAreaView,
-    Dimensions,
-    AppState,
-    Alert,
-    ActivityIndicator, TimePickerAndroid
+    FlatList, ScrollView
   } from 'react-native';
   import { Card } from 'react-native-elements';
   import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
-  import { colors } from '../../../style';
-  import {Divider} from 'react-native-elements';
-import { ScrollView } from 'react-native-gesture-handler';
+import { colors } from '../../../style';
 
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
 
-class LogsScreen extends React.Component{
-    constructor(props){
-        super(props);
-    }
-    
-    render(){
-        const DATA = [
-            {
-              id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-              title: 'First Item',
-            },
-            {
-              id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-              title: 'Second Item',
-            },
-            {
-              id: '58694a0f-3da1-471f-bd96-145571e29d72',
-              title: 'Third Item',
-            },
-          ];
-        return(
-            <View style={{flex:1}}>
-            <ScrollView>
-                <Text style={{fontSize:20,marginTop:hp('2%'),marginLeft:wp('3%')}}>Employee List:</Text>
-            <View style={{marginTop:hp('1%')}}>
-            <Card containerStyle={[styles.cardMainContainer,{backgroundColor:'#fff', marginTop:hp('1%'), elevation:30
-            }]}> 
-            <Card.Title style={styles.cardTitle}>Employee Name                                   Employee Id</Card.Title>                                  
-            </Card>
-            </View>
-            <View style={styles.container}>
-            <FlatList
-            data={DATA}
-            renderItem={({item}) => <Item updated_at={item.updated_at} created_at={item.created_at} duration={item.duration}
-            keyExtractor={item => item.id}
-            />
-                {/* <View style={{flexDirection:'row'}}>
-                <Text style={styles.text}>Kinza</Text>
-                <Text style={{marginLeft:wp('55%')}}>1122</Text>
-                </View>
-                <Divider style={{ backgroundColor: 'blue',marginTop:hp('2%'), marginHorizontal:wp('1%') ,marginBottom:hp('2%')}}/>
-                <View style={{flexDirection:'row'}}>
-                <Text style={styles.text}>Kinza</Text>
-                <Text style={{marginLeft:wp('55%')}}>1122</Text>
-                </View>
-                <Divider style={{ backgroundColor: 'blue',marginTop:hp('2%'), marginHorizontal:wp('1%') , marginBottom:hp('2%')}}/>
-                <View style={{flexDirection:'row'}}>
-                <Text style={styles.text}>Kinza</Text>
-                <Text style={{marginLeft:wp('55%')}}>1122</Text>
-                </View>
-                <Divider style={{ backgroundColor: 'blue',marginTop:hp('2%'), marginHorizontal:wp('1%'), marginBottom:hp('2%') }}/>
-                <View style={{flexDirection:'row'}}>
-                <Text style={styles.text}>Kinza</Text>
-                <Text style={{marginLeft:wp('55%')}}>1122</Text>
-                </View>
-                <Divider style={{ backgroundColor: 'blue',marginTop:hp('2%'), marginHorizontal:wp('1%'), marginBottom:hp('2%') }}/>
-                <View style={{flexDirection:'row'}}>
-                <Text style={styles.text}>Kinza</Text>
-                <Text style={{marginLeft:wp('55%')}}>1122</Text>
-                </View>
-                <Divider style={{ backgroundColor: 'blue',marginTop:hp('2%'), marginHorizontal:wp('1%'), marginBottom:hp('2%') }}/>
-                <View style={{flexDirection:'row'}}>
-                <Text style={styles.text}>Kinza</Text>
-                <Text style={{marginLeft:wp('55%')}}>1122</Text>
-                </View>
-                <Divider style={{ backgroundColor: 'blue',marginTop:hp('2%'), marginHorizontal:wp('1%'), marginBottom:hp('2%') }}/>
-                <View style={{flexDirection:'row'}}>
-                <Text style={styles.text}>Kinza</Text>
-                <Text style={{marginLeft:wp('55%')}}>1122</Text>
-                </View>
-                <Divider style={{ backgroundColor: 'blue',marginTop:hp('2%'), marginHorizontal:wp('1%'), marginBottom:hp('2%') }}/>
-                <View style={{flexDirection:'row'}}>
-                <Text style={styles.text}>Kinza</Text>
-                <Text style={{marginLeft:wp('55%')}}>1122</Text>
-                </View>
-                <Divider style={{ backgroundColor: 'blue',marginTop:hp('2%'), marginHorizontal:wp('1%'), marginBottom:hp('2%') }}/>
-                <View style={{flexDirection:'row'}}>
-                <Text style={styles.text}>Kinza</Text>
-                <Text style={{marginLeft:wp('55%')}}>1122</Text>
-                </View>
-                <Divider style={{ backgroundColor: 'blue',marginTop:hp('2%'), marginHorizontal:wp('1%') ,marginBottom:hp('2%')}}/>
-                <View style={{flexDirection:'row'}}>
-                <Text style={styles.text}>Kinza</Text>
-                <Text style={{marginLeft:wp('55%')}}>1122</Text>
-                </View>
-                <Divider style={{ backgroundColor: 'blue',marginTop:hp('2%'), marginHorizontal:wp('1%'), marginBottom:hp('2%') }}/>
-                <View style={{flexDirection:'row'}}>
-                <Text style={styles.text}>Kinza</Text>
-                <Text style={{marginLeft:wp('55%')}}>1122</Text>
-                </View>
-                <Divider style={{ backgroundColor: 'blue',marginTop:hp('2%'), marginHorizontal:wp('1%') , marginBottom:hp('2%')}}/>
-                <View style={{flexDirection:'row'}}>
-                <Text style={styles.text}>Kinza</Text>
-                <Text style={{marginLeft:wp('55%')}}>1122</Text>
-                </View>
-                <Divider style={{ backgroundColor: 'blue',marginTop:hp('2%'), marginHorizontal:wp('1%'), marginBottom:hp('2%') }}/>
-                <View style={{flexDirection:'row'}}>
-                <Text style={styles.text}>Kinza</Text>
-                <Text style={{marginLeft:wp('55%')}}>1122</Text>
-                </View>
-                <Divider style={{ backgroundColor: 'blue',marginTop:hp('2%'), marginHorizontal:wp('1%'), marginBottom:hp('2%') }}/>
-                <View style={{flexDirection:'row'}}>
-                <Text style={styles.text}>Kinza</Text>
-                <Text style={{marginLeft:wp('55%')}}>1122</Text>
-                </View>
-                <Divider style={{ backgroundColor: 'blue',marginTop:hp('2%'), marginHorizontal:wp('1%'), marginBottom:hp('2%') }}/>
-                <View style={{flexDirection:'row'}}>
-                <Text style={styles.text}>Kinza</Text>
-                <Text style={{marginLeft:wp('55%')}}>1122</Text>
-                </View>
-                <Divider style={{ backgroundColor: 'blue',marginTop:hp('2%'), marginHorizontal:wp('1%'), marginBottom:hp('2%') }}/>
-                <View style={{flexDirection:'row'}}>
-                <Text style={styles.text}>Kinza</Text>
-                <Text style={{marginLeft:wp('55%')}}>1122</Text>
-                </View>
-                <Divider style={{ backgroundColor: 'blue',marginTop:hp('2%'), marginHorizontal:wp('1%'), marginBottom:hp('2%') }}/>
-                <View style={{flexDirection:'row'}}>
-                <Text style={styles.text}>Kinza</Text>
-                <Text style={{marginLeft:wp('55%')}}>1122</Text>
-                </View>
-                <Divider style={{ backgroundColor: 'blue',marginTop:hp('2%'), marginHorizontal:wp('1%'), marginBottom:hp('2%') }}/> */}
-            </View>
-            </ScrollView>
-            </View>
-            
-        );
-    }
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
+
+const LogScreen = () => {
+  const renderItem = ({ item }) => (
+    <Item title={item.title} />
+  );
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
+    </SafeAreaView>
+  );
 }
-export default LogsScreen;
 
 const styles = StyleSheet.create({
-    container: {
-       flex:1,
-       backgroundColor:'#fff',
-       marginHorizontal:hp('2%'),
-       marginBottom:hp('2%'),
-       padding:wp('3%'),
-       elevation:30     
-    },    
-    cardMainContainer:   {                       
-       
-        shadowOpacity: 0.9,
-        shadowRadius: wp('3%'), 
-        // borderRadius:wp('8%')
-    },   
-    cardTitle: {
-        fontSize: 20,
-        color:'#E64A19'
-    },
-    text:{
-        marginLeft:wp('5%')
-    }
-})
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+});
+
+export default LogScreen;
+
--- src/modules/attendenceSystem/settings/settings.js
+++ src/modules/attendenceSystem/settings/settings.js
@@ -3,91 +3,189 @@ import {View,Text,StyleSheet,ScrollView,TextInput,TouchableOpacity,Button} from 
 import { Card } from 'react-native-elements';
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 import {colors} from '../../../style';
-import ActionButton from 'react-native-action-button';
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+import DateTimePicker from '@react-native-community/datetimepicker';
+import DatePicker from 'react-native-custom-datetimepicker'
 
 class SettingScreen extends React.Component{
     constructor(props){
         super(props);
+        this.state={
+          time:'',
+          mode:'date',
+          show:false
+        }
     }
+    onChange = (event, selectedTime) => {
+        const currentTime = selectedTime || time;
+        this.setState({
+            show:Platform.OS === 'ios',
+            time:currentTime
+        })
+      };
+    showMode = (currentMode) => {
+        this.setState({
+            show:true,
+            mode:currentMode
+        })
+      };
+    
+    showTimePicker = () => {
+        this.setState({
+            mode:'time'
+        })
+       
+      };
+    onChangeText1 = (text) => {
+        console.log('done',text)
+    }
+    onChangeText2 = (text) => {
+        console.log('done',text)
+    }
+    onChangeText3 = (time) => {
+        this.setState({
+            time:time
+        })
+        console.log('done',time)
+    }
+    onChangeText4 = (text) => {
+        console.log('done',text)
+    }
+    onChangeText5 = (text) => {
+        console.log('done',text)
+    }
+    onChangeText6 = (text) => {
+        console.log('done',text)
+    }
+    
     render(){
+        console.log('Time',this.state.Time)
         return(
-            <View>           
-            <Card containerStyle={[styles.cardMainContainer,{backgroundColor:'#ff7043', marginTop:hp('1%'), elevation:30, borderRadius:wp('8%')}]}
-            wrapperStyle={{flexDirection:'row'}}>             
-            <Card.Title style={styles.cardTitle}>LEARN: </Card.Title>  
+            <ScrollView horizontal={true}>
+            <Card containerStyle={[styles.cardMainContainer,{backgroundColor:'#ff7043', marginTop:hp('8%'), elevation:30, 
+            borderRadius:wp('8%')}]}
+            >             
+            <Card.Title style={styles.cardTitle}>LEARN: </Card.Title> 
+            <Card.Divider/>  
             <TextInput
-            style={{ height: 40, borderColor: 'gray', borderWidth: 1, justifyContent:'center' ,width:wp('30%'),backgroundColor:'#fff',marginLeft:wp('5%')}}
-            onChangeText={text => onChangeText(text)}
+            style={{ height: hp('6%'),marginTop:hp('10%'),marginLeft:wp('7%'),borderColor: 'white', borderWidth: 1, justifyContent:'center' ,width:wp('60%'),
+            backgroundColor:'#fff',borderRadius:10}}
+            onChangeText={time => this.onChangeText1(time)}
+            placeholder='Employee Name*'
             //value={value}
             />
+          
             <TextInput
-            style={{ height: 40, borderColor: 'gray', borderWidth: 1, justifyContent:'center' ,width:wp('30%'),backgroundColor:'#fff',marginLeft:wp('5%')}}
-            onChangeText={text => onChangeText(text)}
+            style={{ height: hp('6%'),marginTop:hp('2%'),marginLeft:wp('7%'),borderColor: 'white', borderWidth: 1, justifyContent:'center' ,width:wp('60%')
+            ,borderRadius:10,backgroundColor:'#fff'}}
+            onChangeText={text => this.onChangeText2(text)}
+            placeholder='Employee Id*'
             //value={value}
-            />
+             />
+            {/* </Card>  */}
+            
             {/* <Card.Divider/>  */}    
-                                  
-            </Card>
-           
-            <Card containerStyle={[styles.cardMainContainer,{backgroundColor:'#ff5722', marginTop:hp('1%'), elevation:30, borderRadius:wp('8%'),flexDirection:'row'}]} 
-            wrapperStyle={{flexDirection:'row'}}>      
-            <Card.Title style={styles.cardTitle}> Attendance Thresholds: </Card.Title> 
-            <View>
-            <Text style={{color:'#fff',fontSize:19,flexDirection:'row'}}>Time For Late Attendance</Text>   
-            <TextInput
-                placeholder="9:00 AM" 
-                placeholderTextColor="#fff"
-                // style={styles.input}
-                autoCapitalize = 'none'
-                //onChangeText={text => this.setState({ email: text })}
-                //value={value} 
-            />    
-            </View>
-            
-            {/* <Text style={{color:'#fff',fontSize:19}}>Time For Absense</Text>  
-            <TextInput
-                placeholder="9:00 AM" 
-                placeholderTextColor="#fff"
-                // style={styles.input}
-                autoCapitalize = 'none'
-                //onChangeText={text => this.setState({ email: text })}
-                //value={value} 
-            /> */}
-                                           
-            </Card>     
+            <TouchableOpacity style={{marginTop:hp('10%'),width:wp('50%'),height:hp('7%'),backgroundColor:'#fff',borderRadius:15,marginLeft:wp('13%')}}>
+                <Text style={{marginHorizontal:wp('4%'),marginTop:hp('2%'),color:'#ff3d00',fontSize:18}}
+                >
+                Change Mode To Learn
+                </Text>
+            </TouchableOpacity>                  
+             </Card>
 
-            <Card containerStyle={[styles.cardMainContainer,{backgroundColor:'#bf360c', marginTop:hp('1%'), elevation:30, borderRadius:wp('8%')}]}
-            wrapperStyle={{flexDirection:'row'}}>             
-            <Card.Title style={styles.cardTitle}>AutoMode Switching: </Card.Title>  
-            
-            {/* <Card.Divider/>  */}   
-            <TouchableOpacity style={{marginTop:hp('3%'),width:wp('30%'),height:hp('7%'),backgroundColor:'#fff',borderRadius:15,marginLeft:wp('10%')}}>
-                <Text style={{marginHorizontal:wp('4%'),marginTop:hp('2%'),color:'#ff3d00',fontSize:18}}>
+            <Card containerStyle={[styles.cardMainContainer,{backgroundColor:'#bf360c', marginTop:hp('8%'), elevation:30, borderRadius:wp('8%')}]}
+            >             
+            <Card.Title style={styles.cardTitle}> Attendance Thresholds: </Card.Title>
+            <Card.Divider/>    
+            <Text style={{color:'#fff',marginLeft:wp('4%'),marginTop:hp('5%')}}>Time For Late Attendance</Text>
+            <TouchableOpacity onPress={()=>this.showTimePicker()}>
+            <Text style={{alignSelf:'flex-end',color:'#fff'}}>Set Time</Text>
+           </TouchableOpacity>
+           <Text>{this.state.Time}</Text>
+            {/* <TextInput
+            style={{ height:hp('5%'), borderRadius: 10, justifyContent:'center' ,width:wp('60%'),
+            borderColor:'#fff',marginLeft:wp('5%')}}
+            defaultValue={this.state.Time}
+            placeholderTextColor="#fff"
+            underlineColorAndroid="#fff"
+            //onChangeText={time => this.onChangeText3(time)}
+            //value={value}
+            /> */}
+            {/* <DateTimePickerModal
+            isVisible={this.state.isDatePickerVisible}
+            mode="time"
+            onConfirm={(time)=>this.handleConfirm(time)}
+            onCancel={this.hideDatePicker}
+            /> */}
+             {/* <DateTimePicker
+          testID="dateTimePicker"
+          value={time}
+          mode={this.state.mode}
+          is24Hour={true}
+          display="default"
+          onChange={(event, selectedTime)=> this.onChange(event, selectedTime)}
+        /> */}
+             <Text style={{color:'#fff',marginLeft:wp('4%'),marginTop:hp('5%')}}>Time For Absense</Text>
+            <TextInput
+            style={{ height:hp('5%'), borderRadius: 10, justifyContent:'center' ,width:wp('60%'),
+            borderColor:'#fff',marginLeft:wp('5%')}}
+            placeholder="12:00 PM" 
+            placeholderTextColor="#fff"
+            underlineColorAndroid="#fff"
+            onChangeText={text => this.onChangeText4(text)}
+            //value={value}
+            /> 
+            <TouchableOpacity style={{marginTop:hp('10%'),width:wp('50%'),height:hp('7%'),backgroundColor:'#fff',borderRadius:15,marginLeft:wp('13%')}}
+             onPress={()=>console.log('Thresholds')}>
+                <Text style={{marginHorizontal:wp('4%'),marginTop:hp('2%'),color:'#ff3d00',fontSize:18,paddingLeft:wp('9%')}}>
                 Submit Times
                 </Text>
-            </TouchableOpacity>                                            
-            </Card>
-
-            <Card containerStyle={[styles.cardMainContainer,{backgroundColor:'#ff3d00', marginTop:hp('1%'), elevation:30,
-            paddingTop:hp('3%'),borderRadius:wp('8%')}]}
-            wrapperStyle={{flexDirection:'row'}}>             
-            <Card.Title style={styles.cardTitle}>Current Mode: </Card.Title>  
-            <Text style={{marginHorizontal:hp('1%'),marginTop:hp('4.1%'),fontSize:20,color:'#fff'}}>Check in</Text>
-            {/* <Card.Divider/>  */}   
-            <TouchableOpacity style={{marginTop:hp('3%'),width:wp('40%'),height:hp('7%'),backgroundColor:'#fff',borderRadius:15,marginLeft:wp('5%')}}>
-                <Text style={{marginHorizontal:wp('4%'),marginTop:hp('2%'),color:'#ff3d00',fontSize:18}}>
-                    Change To Check Out
+            </TouchableOpacity>                 
+             </Card>
+             <Card containerStyle={[styles.cardMainContainer,{backgroundColor:'#ff3d00', marginTop:hp('8%'), elevation:30, borderRadius:wp('8%')}]}
+            >             
+            <Card.Title style={styles.cardTitle}> AutoMode Switching: </Card.Title>
+            <Card.Divider/>    
+            <Text style={{color:'#fff',marginLeft:wp('4%'),marginTop:hp('5%')}}>Check In Time Auto Switch</Text>
+            <TextInput
+            style={{ height:hp('5%'), borderRadius: 10,  justifyContent:'center' ,width:wp('60%'),
+            borderColor:'#fff',marginLeft:wp('5%')}}
+            placeholder="7:08 AM" 
+            placeholderTextColor="#fff"
+            underlineColorAndroid="#fff"
+            onChangeText={text => this.onChangeText5(text)}
+            //value={value}
+            />
+             <Text style={{color:'#fff',marginLeft:wp('4%'),marginTop:hp('5%')}}>Check Out Time Auto Switch</Text>
+            <TextInput
+            style={{ height:hp('5%'), borderRadius: 10,  justifyContent:'center' ,width:wp('60%'),
+            borderColor:'#fff',marginLeft:wp('5%')}}
+            placeholder="2:18 AM" 
+            placeholderTextColor="#fff"
+            underlineColorAndroid="#fff"
+            onChangeText={text => this.onChangeText6(text)}
+            //value={value}
+            /> 
+            <TouchableOpacity style={{marginTop:hp('10%'),width:wp('50%'),height:hp('7%'),backgroundColor:'#fff',borderRadius:15,marginLeft:wp('13%')}}>
+                <Text style={{marginHorizontal:wp('4%'),marginTop:hp('2%'),color:'#ff3d00',fontSize:18,paddingLeft:wp('9%')}}>
+                Submit Times
                 </Text>
-            {/* <Button
-            onPress={console.log('Kinza')}
-            title="Learn More"
-            style={{width:wp('30%'),height:hp('30%')}}
-            color="#000"
-            accessibilityLabel="Learn more about this purple button"
-            /> */}
-            </TouchableOpacity>                                                
-            </Card>      
-            </View>
+            </TouchableOpacity>                 
+             </Card>
+             
+             <Card containerStyle={[styles.cardMainContainer,{backgroundColor:'#ff5722', marginTop:hp('8%'), elevation:30, borderRadius:wp('8%')}]}
+            >             
+            <Card.Title style={styles.cardTitle}>Current Mode: </Card.Title>  
+            <Card.Divider/> 
+            <Text style={{alignSelf:'center',fontSize:30,color:'#fff',marginTop:hp('10%')}}>Check in</Text>     
+            <TouchableOpacity style={{marginTop:hp('10%'),width:wp('50%'),height:hp('7%'),backgroundColor:'#fff',borderRadius:15,marginLeft:wp('13%')}}>
+                <Text style={{marginHorizontal:wp('1%'),marginTop:hp('2%'),color:'#ff3d00',fontSize:18,paddingLeft:wp('5%')}}>
+                Change To Check Out
+                </Text>
+            </TouchableOpacity>                                    
+             </Card>
+            </ScrollView>          
+            
         );
     }
 }
@@ -102,11 +200,13 @@ const styles = StyleSheet.create({
         
     },
      cardMainContainer:{ 
-         width:wp('95%'),
-         height:hp('20%'),                     
+         flex:1,
+         width:wp('86%'),
+         height:hp('75%'),
+         marginTop:hp('30%'),
          elevation:60,
          shadowOpacity: 0.9,
          shadowRadius: wp('3%'), 
-         borderRadius:wp('8%')
+         borderRadius:wp('8%'),
      },
 })
